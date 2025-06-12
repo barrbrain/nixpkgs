36e6e014b3be9662bc3fdf65da1d24994067bf68
David Michael Barr <b@rr-dav.id.au>
2025-06-13 04:07:05 +0900

    Qt6 qtwebengine: Fix shebang substitution for linker_ulimit.sh

--- pkgs/development/libraries/qt-6/modules/qtwebengine/default.nix
+++ pkgs/development/libraries/qt-6/modules/qtwebengine/default.nix
@@ -135,6 +135,9 @@ qtModule {
       patchShebangs .
     )
 
+    substituteInPlace cmake/QtConfigureHelpers.cmake \
+      --replace "/bin/bash" "${buildPackages.bash}/bin/bash"
+
     substituteInPlace cmake/Functions.cmake \
       --replace "/bin/bash" "${buildPackages.bash}/bin/bash"
 
